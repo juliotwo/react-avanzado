035d198834549d90dabf2f427b533f56e23514d4
Julio vargas <juliotwo1996@gmail.com>
2020-12-09 22:56:52 -0600

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HTMLWebpackplugin = require('html-webpack-plugin')
+module.exports = {
+  output: {
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new HTMLWebpackplugin({
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+}
